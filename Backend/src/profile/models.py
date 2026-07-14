e3db00e1f7058517293b80cb82d5d7fd7e86520d
Aayansh-singh-24 <luciferversion2.2@gmail.com>
2026-07-14 19:42:12 +0530

    profile/model file improvement

---
+++ Backend/src/profile/models.py
@@ -0,0 +1,15 @@
+from src.utils.db import Base
+from sqlalchemy import Column, Integer, String, ForeignKey
+from sqlalchemy.orm import relationship, Mapped,  mapped_column
+from src.user.models import UserModel
+
+# This is way to write model in sqlalchemy version 2.x 
+
+class ProfileModel(Base):
+    __tablename__ = "profile_table"
+    id : Mapped[int] = mapped_column(Integer, primary_key=True, nullable=False)
+    filename : Mapped[str] = mapped_column(String,nullable=False)
+    filepath : Mapped[str] = mapped_column(String,nullable=False)
+    user_id : Mapped[str] = mapped_column(Integer,ForeignKey("user_table.id",ondelete="CASCADE"))
+
+    owner : Mapped[UserModel] = relationship("UserModel",back_populates="profile")
